8fc3cff66cb55ec545f928d73a319c7eb259a811
artyomof <125359982+artyomof@users.noreply.github.com>
2026-06-30 09:45:38 +0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const listItems = document.querySelectorAll("ul li");
     listItems.forEach(item => {
         item.addEventListener("mouseenter", () => {
-            item.style.border = "2px solid red"; // цвет рамки при наведении
+            item.style.border = "2px solid red";
         });
         item.addEventListener("mouseleave", () => {
-            item.style.border = "1px solid transparent"; // убираем рамку
+            item.style.border = "1px solid transparent";
         });
     });
 });
