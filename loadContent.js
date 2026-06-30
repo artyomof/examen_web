1c0ba53f6da6d832cf74113fb46c9808fe606d76
artyomof <125359982+artyomof@users.noreply.github.com>
2026-06-30 10:35:16 +0300

    Update loadContent.js

--- loadContent.js
+++ loadContent.js
@@ -1,17 +1,15 @@
-document.getElementById('loadButton').addEventListener('click', loadContent);
-loadContent(){
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'content.html', true);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      document.getElementById('content-area').innerHTML = xhr.responseText;
-    } 
-    else {
-      console.error('Ошибка загрузки файла:', xhr.status);
-    }
-  };
-  xhr.onerror = function() {
-    console.error('Ошибка запроса');
-  };
-  xhr.send();
-}
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'content.html', true);
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    document.getElementById('content-area').innerHTML = xhr.responseText;
+  } 
+  else {
+    console.error('Ошибка загрузки файла:', xhr.status);
+  }
+};
+xhr.onerror = function() {
+  console.error('Ошибка запроса');
+};
+xhr.send();
+
